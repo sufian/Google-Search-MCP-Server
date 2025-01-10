6dc85aff089890002defae2dda4d2e27a7998769
mixelpixx <chris_stockslager@comcast.net>
2025-01-09 23:35:22 -0500

    Update start-servers.cmd

--- start-servers.cmd
+++ start-servers.cmd
@@ -9,6 +9,6 @@ REM Wait a moment for Python servers to initialize
 timeout /t 2
 
 REM Start MCP server with environment variables
-set GOOGLE_API_KEY=AIzaSyDfkGHk5g4sK89zpzS3fJ6a9GoGnoJh8YU
-set SEARCH_ENGINE_ID=96881945019f042be
+set GOOGLE_API_KEY=
+set SEARCH_ENGINE_ID=
 node dist/google-search.js
